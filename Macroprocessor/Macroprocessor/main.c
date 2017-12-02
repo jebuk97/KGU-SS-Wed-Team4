b5967ae88fd8ea55c9b4ea3993be319e81ddce80
iz100412 <iz100412@gmail.com>
2017-12-02 18:54:10 +0900

    수정
    ㅇ

--- Macroprocessor/Macroprocessor/main.c
+++ Macroprocessor/Macroprocessor/main.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+
+#define _CRT_SECURE_NO_WARNINGS
 
 #define TRUE 1
 #define FALSE 0
@@ -45,11 +48,11 @@ typedef struct NAMTAB {
 int expanding;// TRUE, FALSE만을 저장하는 조건변수
 int top = 0;//PROTOTYPE, NAMTAB의 아직 채워지지 않은 배열의 위치를 가리킨다.
 
-			//DEFTAB과 NAMTAB을 가지고 다른 함수를 호출하는 함수, 가지고있는 자료구조만 일단 적어놓음
+//DEFTAB과 NAMTAB을 가지고 다른 함수를 호출하는 함수
 void macro_processor(FILE *in, FILE *out);
 
 //expanding의 값에 따라 DEFTAB에서 문자열을 읽거나 input된 FILE을 읽어오는 함수
-Line getLine(FILE *in, Line *pline);
+Line getLine(FILE *in, PROTOTYPE *p, Line *pline);
 
 //getline에서 읽어온 문자열로 expand나 define 함수를 호출하거나 expand된 소스를 작성하는 함수
 void processline(FILE *in, FILE *out, DEFTAB *def, NAMTAB *name, Line *line);
@@ -58,10 +61,10 @@ void processline(FILE *in, FILE *out, DEFTAB *def, NAMTAB *name, Line *line);
 void expand(FILE *in, FILE *out, DEFTAB *def, NAMTAB *name, Line *line);
 
 //macro로부터 NAMTAB과 DEFTAB에 이름과 정의를 set하고 한줄씩 읽어 DEFTAB에 저장, NAMTAB에 시작과 끝의 위치를 저장한다
-void define(char *macro, DEFTAB *def, NAMTAB *name, FILE *in);
+void define(Line *macro, DEFTAB *def, NAMTAB *name, FILE *in);
 
 //주석인지 아닌지 검사하는 함수
-int is_comment_line(char *buf);
+int is_comment_line(Line *buf);
 
 //line의 opcode가 주어진 문자열과 같은지 비교하는 함수
 int compare_opcode(Line *line, char* buf);
@@ -71,7 +74,7 @@ Line *getlinePtr = NULL;
 //문자열을 찾아 치환해주는 함수
 char *replaceAll(char *s, const char *olds, const char *news);
 
-void main() {
+int main() {
 	FILE *input, *output;
 	fopen_s(&input, "INPUT.txt", "r");
 	fopen_s(&output, "OUTPUT.txt", "w");
@@ -85,6 +88,12 @@ void main() {
 void macro_processor(FILE *in, FILE *out) {
 	DEFTAB definetable;
 	NAMTAB nametable[MACRO_MAX];
+	expanding = FALSE;
+	Line buf;
+	while (strcmp(buf.opcode, "END")) {
+		buf = getLine(in, NULL, NULL);
+		//processline(in, out, &definetable, &nametable[top], &buf);
+	}
 }
 
 void define(Line* macro, DEFTAB *def, NAMTAB *name, FILE *in) {
@@ -110,7 +119,7 @@ void define(Line* macro, DEFTAB *def, NAMTAB *name, FILE *in) {
 
 	level = 1;
 	while (level > 0) {
-		get = getLine(in, NULL);
+		get = getLine(in, NULL, NULL);
 		if (!is_comment_line(macro)) {
 
 			//처음것이 나오면 ?1로 치환, ?2로 치환...
@@ -125,7 +134,7 @@ void define(Line* macro, DEFTAB *def, NAMTAB *name, FILE *in) {
 				}
 				printf("%s\n", get.address);
 			}
-			
+
 			//받은 line을 def에 저장
 			def->line[index] = get;
 			index++;
@@ -144,22 +153,21 @@ void define(Line* macro, DEFTAB *def, NAMTAB *name, FILE *in) {
 
 }
 
-Line getLine(FILE *in, Line *pline)
+Line getLine(FILE *in, PROTOTYPE *p, Line *pline)
 {
-	Line line = { 0, };
+	Line line;
 	printf("hello getLine\n");
-	int temp=0;
+	int temp = 0;
 	if (expanding == TRUE) {
-		fscanf(in, "%d\t", &temp);
-		fscanf(in, "%s\t%s\t%s\t", line.address, line.label, line.opcode);
+		line = *pline;
+		replaceAll(line.address, "?1", p->argtab[0].argument);
+		replaceAll(line.address, "?2", p->argtab[1].argument);
+		replaceAll(line.address, "?3", p->argtab[2].argument);
 		printf("%s\t%s\t%s\n", line.address, line.label, line.opcode);
 	}
 	else {
-		//printf("%s\n", pline);
-		//테스트용
-		return *pline;
+		fscanf(in, "%s%s%s", &line.label, &line.opcode, &line.address);
 	}
-	
 	return line;
 }
 
@@ -195,4 +203,16 @@ char *replaceAll(char *s, const char *olds, const char *news) {
 	*sr = '\0';
 
 	return result;
+}
+
+int is_comment_line(Line *buf) {
+	if (buf->label[0] == '.')
+		return 1;
+	return 0;
+}
+
+int compare_opcode(Line *line, char* buf) {
+	if (!strcmp(line->opcode, buf))
+		return 1;
+	return 0;
 }
